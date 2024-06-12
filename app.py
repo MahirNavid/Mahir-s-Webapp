6a10e38749462ec8b99d66b38af19d46a3541b83
MahirNavid <161045600+MahirNavid@users.noreply.github.com>
2024-06-12 16:36:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template, request
 import yfinance as yf
+from pytickersymbols import PyTickerSymbols
 
 app = Flask(__name__)
 
-TICKER = [
-    "NVDA",
-    "AAPL",
-    "AMZN",
-    "META",
-    "TSLA",
-    "AMD",
-    "GM"
-]
+stock_data = PyTickerSymbols()
+sp100_stocks = list(stock_data.get_stocks_by_index('S&P 100'))
+TICKER = []
+for stock in sp100_stocks[:30]:
+    TICKER.append(stock['symbol'])
+
+PAGE = [1,2,3]
 
 @app.route("/")
 def index():
-    return render_template("index.html", ticker=TICKER)
+    return render_template("index.html")
+
+@app.route("/ticker_list")
+def ticker_list():
+    test = request.args.get('page', 1, type=int)
+    upper = test*10
+    lower = upper-10
+    ticker=TICKER[lower:upper]
+    return render_template("ticker_list.html", ticker=ticker, page=PAGE, test=test)
 
 # https://github.com/ranaroussi/yfinance/tree/main
 
-@app.route("/ticker_details")
+@app.route("/ticker_details", methods=["GET"])
 def ticker_details():
     selected_ticker = request.args.get('ticker')
     ticker_data = yf.Ticker(selected_ticker)
